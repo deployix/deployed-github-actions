8f16f6955d35cbc4e5b753156184a06f10257709
tony-macbook-pro <tonymbugua1@gmail.com>
2024-06-19 19:16:36 -0700

    print

--- cmd/deployed-github-actions/main.go
+++ cmd/deployed-github-actions/main.go
@@ -144,11 +144,16 @@ func main() {
 	// }
 	pushOptions := git.PushOptions{
 		Progress: os.Stdout,
-		Auth: &http.TokenAuth{
-			Token: input.GitHubToken,
+		Auth: &http.BasicAuth{
+			Username: "deployix",
+			Password: input.GitHubToken,
 		},
 	}
 
+	email := os.Getenv("INPUT_USER_EMAIL")
+
+	fmt.Println("EMAIL: " + email[0:5])
+	fmt.Println("RR: " + input.GitHubToken[0:5])
 	fmt.Println("push")
 	err = r.Push(&pushOptions)
 	if err != nil {
